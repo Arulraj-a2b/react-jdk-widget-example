268d0930bd94d9e6a2dc944689fdb252d9e13431
Arulraj <abofficial1997@gmail.com>
2024-11-13 18:01:48 +0530

    feat add html props

--- src/jsWidget.tsx
+++ src/jsWidget.tsx
@@ -4,8 +4,11 @@ import ChatbotWidget from "./modules/WidgetModule/ChatbotWidget";
 window.addEventListener("load", () => {
   const container = document.getElementById("chatbot-container");
   if (container && ReactDOM.createRoot) {
+    const title = container.getAttribute("data-title") || "Bot"; // Default to "Bot" if not set
+    const themeColor = container.getAttribute("data-theme-color") || "#00bfa5"; // Default theme color
+
     const root = ReactDOM.createRoot(container);
-    root.render(<ChatbotWidget title="Support Bot" themeColor="#00bfa5" />);
+    root.render(<ChatbotWidget title={title} themeColor={themeColor} />);
   } else {
     console.error("ReactDOM is not available or the container doesn't exist.");
   }
